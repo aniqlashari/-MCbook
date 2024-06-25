212e03f0958ee55624b529d6d61bfdbcd2ddadb6
aniqlashari <mahaqamber3@gmail.com>
2024-06-25 21:04:50 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+alert ("hard work again")
+
+var num1=+ prompt ("Enter any number first","")
+
+
+var num2=+ prompt ("Enter any number second","")
+
+
+var num3=+ prompt ("Enter any number third","")
+
+alert  (num1+num2+num3)
+alert  (num1-num2-num3)
+alert  (num1*num2*num3)
+alert  (num1/num2/num3)
